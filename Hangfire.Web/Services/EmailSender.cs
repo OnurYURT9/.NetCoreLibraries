714d6e17c5db83b00473bc8f59962a311898f709
ONUR YURT <57957917+OnurYURT9@users.noreply.github.com>
2023-01-01 03:10:10 +0300

    Add files via upload

---
+++ Hangfire.Web/Services/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire.Web.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task Sender(string userId, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
