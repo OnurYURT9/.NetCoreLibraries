44f6f9d1b629dfcd0f228dac8d8446f9641f7030
ONUR YURT <57957917+OnurYURT9@users.noreply.github.com>
2023-01-01 03:14:54 +0300

    Add files via upload

---
+++ ErrorHandling/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using ErrorHandling.Filter;
+using ErrorHandling.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Controllers
+{
+    [CustomHandleExceptionFilterAttribute(ErrorPage ="Hata1")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //throw new Exception("Veri tabanı bağlanırken hata meydana geldi");
+            int a = 5;
+            int b = 0;
+            int result = a / b;
+            return View();
+        }
+        [CustomHandleExceptionFilterAttribute(ErrorPage = "Hata2 ")]
+        public IActionResult Privacy()
+        {
+           
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.path = exception.Path;
+            ViewBag.Message = exception.Error.Message.ToString();
+            return View();
+        }
+        public IActionResult Hata1()
+        {
+            return  View();
+        }  
+        public IActionResult Hata2()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErrorHandling/Filter/CustomHandleExceptionFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Filter
+{
+    public class CustomHandleExceptionFilterAttribute:ExceptionFilterAttribute
+    {
+        public string ErrorPage { get; set; }
+        public override void OnException(ExceptionContext context)
+        {
+            var result = new ViewResult() { ViewName = ErrorPage };
+            result.ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), context.ModelState); //bu kodu yazmazsak hata1 sayfasına data gönderemeyiz
+            
+            result.ViewData.Add("Exception", context.Exception);
+            result.ViewData.Add("Url", context.HttpContext.Request.Path.Value);
+            context.Result = result;
+        }
+    }
+}
